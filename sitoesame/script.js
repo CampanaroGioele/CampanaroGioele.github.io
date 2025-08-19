1ca4633573b49e048be6ec4fea9c1cee70708971
CampanaroGioele <166509763+CampanaroGioele@users.noreply.github.com>
2025-08-19 11:57:22 +0200

    Update script.js

--- sitoesame/script.js
+++ sitoesame/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
        if((username==utente2 )&&( password==password2 ))
       pvd=true;
        
-      if (user && pvd)  {
+      if (user=true && pvd=true)  {
         alert("Login effettuato con successo!");
       } else {
         alert("Utente non registrato");
@@ -49,4 +49,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
 
